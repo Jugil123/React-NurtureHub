5b3b6e25e783fe1adc900eb82a110a962d4df9a9
Andreiyaa1 <143610003+Andreiyaa1@users.noreply.github.com>
2023-11-19 18:50:39 +0800

    changes

--- src/UserType.js
+++ src/UserType.js
@@ -1,15 +1,8 @@
-import { useCallback } from "react";
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./UserType.css";
 
 const UserType = () => {
-  const onVectorClick = useCallback(() => {
-    // Please sync "Login" to the project
-  }, []);
-
-  const onGroupContainerClick = useCallback(() => {
-    // Please sync "RegisterCaregiver" to the project
-  }, []);
-
   return (
     <div className="usertype">
       <div className="are-you-caregiver">Are you Caregiver or Recipient?</div>
@@ -17,17 +10,19 @@ const UserType = () => {
         className="vector-icon"
         alt=""
         src="/vector.svg"
-        onClick={onVectorClick}
       />
-      <div className="button-container" onClick={onGroupContainerClick}>
-      <button className="button-container">
-        <div className="button">Caregiver</div>
-      </button>
-    </div>
-    <div className="button-container" onClick={onGroupContainerClick}></div>
-      <button className="button-container">
-        <div className="button">Recipient</div>
-      </button>
+      <div className="button-container">
+        {/* Use Link to navigate to the "RegisterCaregiver" route */}
+        <Link to="/register-caregiver">
+          <button className="button">Caregiver</button>
+        </Link>
+      </div>
+      <div className="button-container">
+        {/* Use Link to navigate to the "RegisterRecipient" route */}
+        <Link to="/register-recipient">
+          <button className="button">Recipient</button>
+        </Link>
+      </div>
     </div>
   );
 };
