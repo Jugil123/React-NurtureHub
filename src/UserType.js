deebc57cd4e83f5a4a587f8daa791cf24aa9c4ad
Juspher123 <juspherpatena@gmail.com>
2023-11-21 16:27:52 +0800

    Update UI with Landing Page

--- src/UserType.js
+++ src/UserType.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from "react";
+import "./UserType.css";
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 const UserType = () => {
   const onVectorClick = useCallback(() => {
     // Please sync "Login" to the project
   }, []);
 
-  const handleTypeSelection = (type) => {
-    setSelectedType(type);
-  };
+  const onGroupContainerClick = useCallback(() => {
+    // Please sync "RegisterCaregiver" to the project
+  }, []);
 
   return (
-    <div>
-      <h2>Choose User Type</h2>
-      <button onClick={() => navigate('/register-recipient')}>Recipient</button>
-      <button onClick={() => navigate('/register-caregiver')}>Caregiver</button>
-
-      
+    <div className="usertype">
+      <div className="are-you-caregiver">Are you Caregiver or Recipient?</div>
+      <img
+        className="vector-icon"
+        alt=""
+        src="/vector.svg"
+        onClick={onVectorClick}
+      />
+      <div className="button-container" onClick={onGroupContainerClick}>
+      <button className="button-container" style={{left: "327px", top: "6px"}}>
+      <Link to="/register-caregiver">
+        <div className="button">Caregiver</div>
+        </Link>
+      </button>
+    </div>
+    <div className="button-container" onClick={onGroupContainerClick}></div>
+      <button className="button-container" style={{top: "471px"}}>
+      <Link to="/register-recipient">
+        <div className="button">Recipient</div>
+        </Link>
+      </button>
     </div>
   );
 };
