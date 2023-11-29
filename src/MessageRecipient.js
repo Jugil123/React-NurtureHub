95b7c6b444cc3e2d9b351684529a997aa6b61be6
Juspher123 <juspherpatena@gmail.com>
2023-11-29 20:55:15 +0800

    Create MessageRecipient.js

---
+++ src/MessageRecipient.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+import styles from './HomeRecipient.module.css';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Extract userObject from location state
+  const userObject = location.state ? location.state.userObject : null;
+
+  useEffect(() => {
+    // Perform any initial setup using userObject if needed
+    console.log('userObject:', userObject);
+  }, [userObject]);
+
+  const handleSearch = async () => {
+    setSearchResults([]);
+    console.log('Search Term:', searchTerm);
+    try {
+      // Make an HTTP GET request to the backend API with the search term
+      const response = await axios.get(`http://localhost:8080/caregiver/searchCaregiver?searchString=${searchTerm}`);
+      setSearchResults(response.data); // Update search results with the data from the backend
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const navigateToMyProfile = () => {
+    navigate('/my-profile', { state: { userObject } });
+  };
+
+  const navigateToViewCaregiver = (userId) => {
+    navigate(`/view-caregiver/${userId}`, { state: { userObject } });
+  };
+
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.navColumn}>
+        <div className={styles.logoContainer}>
+          <img src="/nurturehublogo-2@2x.png" alt="App Logo" className={styles.appLogo} />
+        </div>
+        <div onClick={navigateToMyProfile} className={styles.userProfileContainer}>
+          <img src="/sample.png" alt="Profile" className={styles.userProfilePicture} />
+          <div>
+            {userObject ? (
+              <p className={styles.userProfileInfo}>{`${userObject.firstname} ${userObject.lastname}`}</p>
+            ) : (
+              <p className={styles.userProfileInfo}>Loading...</p>
+            )}
+          </div>
+        </div>
+        <div>
+          <ul className={styles.navLinksContainer}>
+            <li>
+              <a href="/" className={`${styles.navLink} ${styles.activeNavLink}`}>
+                <img src="/home-icon.svg" alt="Home" className={`${styles.navIcon} ${styles.activeNavLinkIcon}`} /> Home
+              </a>
+            </li>
+            <li>
+              <a href="/messages" className={styles.navLink}>
+                <img src="/messages-icon.svg" alt="Messages" className={styles.navIcon} /> Messages
+              </a>
+            </li>
+            <li>
+              <a href="/records" className={styles.navLink}>
+                <img src="/records-icon.svg" alt="Records" className={styles.navIcon} /> Records
+              </a>
+            </li>
+            <li>
+              <a href="/login" className={styles.navLink}>
+                <img src="/logout-icon.svg" alt="Logout" className={styles.navIcon} /> Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.contentColumn}>
+        <div className={styles.searchBarContainer}>
+          <input type="text" placeholder="Search users..." className={styles.searchInput} value={searchTerm} onChange={handleSearchInputChange}/>
+          <button className={styles.searchButton} onClick={handleSearch}>
+            <img src="/search-icon.svg" alt="Search" className={styles.searchIcon} />
+          </button>
+        </div>
+        {searchResults.map((user) => (
+          <div
+            key={user.id}
+            className={styles.userProfileContainer}
+            onClick={() => navigateToViewCaregiver(user.caregiverId)}
+          >
+            <img src={user.profilePicture} alt="Profile" className={styles.userProfilePicture} />
+            <div>
+              <p className={styles.userProfileInfo}>{`${user.firstname} ${user.lastname}`}</p>
+              <p className={styles.userProfileInfo}>{`Address: ${user.address}`}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
